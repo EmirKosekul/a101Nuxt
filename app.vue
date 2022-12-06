92b091326f327e722607ed7d74b918d1f8529e31
Emir <emir_kskul@hotmail.com>
2022-12-07 01:49:59 +0300

    First commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+    <headera101/>
+    
+</template>
+
+<style>
+
+
+</style>
+
+
+<script>
+import headera101 from "./components/header.vue"
+
+
+export default{
+ 
+  components:{
+    headera101,
+  },
+}
+
+</script>
