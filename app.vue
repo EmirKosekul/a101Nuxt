130455c8e0c929e675241045c3a9194907ef0aab
Emir <emir_kskul@hotmail.com>
2022-12-07 05:00:24 +0300

    commit

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
     <headera101/>
-    
+    <ucuz/>
+    <kategoriler1/>
+    <kategoriler2/>
+    <oneCikanlar/>
 </template>
 
 <style>
@@ -11,12 +14,19 @@
 
 <script>
 import headera101 from "./components/header.vue"
-
+import ucuz from "./components/ucuz.vue"
+import kategoriler1 from "./components/kategoriler1.vue"
+import kategoriler2 from "./components/kategoriler2.vue"
+import oneCikanlar from "./components/oneCikanlar.vue"
 
 export default{
  
   components:{
     headera101,
+    ucuz,
+    kategoriler1,
+    kategoriler2,
+    oneCikanlar,
   },
 }
 
---
+++ components/ucuz.vue
@@ -0,0 +1,94 @@
+<template>
+<div class="sepeteEkleKartları">
+            <div class="ucuz">
+                <div>
+                    <h1>Büyük olduğu için UCUZ</h1>
+                    <span>Tümünü Gör ></span>
+                </div>
+                <div class="cards">
+                    <div class="card">
+                        <img src="https://pictures.personaclick.com/resize-images/220/c8b6a5946225d5fbdc1570119c3aab/2400803345213161865.jpg">
+                        <a>Pınar Süt Yağlı 200ml</a>
+                        <a>10.90TL</a>
+                        <button><i class="fa-solid fa-cart-shopping"></i> Sepete Ekle</button>
+                    </div>
+                    <div class="card">
+                        <img src="https://pictures.personaclick.com/resize-images/220/c8b6a5946225d5fbdc1570119c3aab/221128031.jpg">
+                        <a>Gofret Bitter Çikolatalı 33g</a>
+                        <a>14.90TL</a>
+                        <button><i class="fa-solid fa-cart-shopping"></i> Sepete Ekle</button>
+                    </div>
+                    <div class="card">
+                        <img src="https://pictures.personaclick.com/resize-images/220/c8b6a5946225d5fbdc1570119c3aab/2400803345750032703.jpg">
+                        <a>Çiçeğim Kireç Çözücü 1L</a>
+                        <a>18.90TL</a>
+                        <button><i class="fa-solid fa-cart-shopping"></i> Sepete Ekle</button>
+                    </div>
+                    <div class="card">
+                        <img src="https://pictures.personaclick.com/resize-images/220/c8b6a5946225d5fbdc1570119c3aab/2400803345750032703.jpg">
+                        <a>Çiçeğim Kireç Çözücü 1L</a>
+                        <a>18.90TL</a>
+                        <button><i class="fa-solid fa-cart-shopping"></i> Sepete Ekle</button>
+                    </div>
+                    <div class="card">
+                        <img src="https://pictures.personaclick.com/resize-images/220/c8b6a5946225d5fbdc1570119c3aab/2400803345750032703.jpg">
+                        <a>Çiçeğim Kireç Çözücü 1L</a>
+                        <a>18.90TL</a>
+                        <button><i class="fa-solid fa-cart-shopping"></i> Sepete Ekle</button>
+                    </div>
+                </div>
+            </div>
+        </div> 
+</template>
+
+<style>
+.sepeteEkleKartları h1{
+    display: inline-block;
+    color:#00abc7;
+    font-weight: bolder;
+    font-family:Arial;
+ }
+ .sepeteEkleKartları span{
+    color:#fddd00;
+    font-family:Arial;
+ }
+ .sepeteEkleKartları .cards .card{
+    display: inline-block;
+    background-color: #f5f5f5;
+    margin:0.125cm;
+    width: 220px;
+    height: 400px;
+ }
+ .sepeteEkleKartları .cards .card img{
+    height: 5cm;
+    display: block;
+    margin-left: 0.4cm;
+    margin-top:0.3cm
+ }
+ .sepeteEkleKartları .cards .card a{
+    display:block;
+    text-align: center;
+    margin:1cm; 
+ }
+ .sepeteEkleKartları .cards .card button{
+    background-color: #00abc7;
+    color:white;
+    border:none;
+    padding: 5px;
+    width: 100px;
+    margin-left:1.5cm;
+ }
+ .sepeteEkleKartları{
+    margin-left: 5cm;
+ }
+</style>
+
+<script>
+  import VueSlickCarousel from 'vue-slick-carousel'
+  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+  export default {
+    name: 'MyComponent',
+    components: { VueSlickCarousel },
+  }
+</script>
